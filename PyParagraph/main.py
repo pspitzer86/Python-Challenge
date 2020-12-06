b58ec7b5f5c72006e40c9bcb87e9a0d560c09bf1
Patrick Spitzer <rick.spitzer@yahoo.com>
2020-12-06 16:50:05 -0700

    Counted words and sentences

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -6,11 +6,19 @@ my_paragraph = "C:\\Python-Challenge\\PyParagraph\\Resources\\paragraph_2.txt"
 # Open the file in "read" mode ('r') and store the contents in the variable "text"
 with open(my_paragraph, 'r') as text:
 
-    # This stores a reference to a file stream
-    print(text)
-
     # Store all of the text inside a variable called "lines"
     lines = text.read()
 
     # Print the contents of the text file
-    print(lines)
+    #print(lines)
+
+    Word_Split = lines.split()
+    Word_Count = len(Word_Split)
+    
+    Sentence_Count = len(re.findall(r'\.', lines))
+    #Sentence_Split = lines.split(".")
+    #Sentence_Count = len(Sentence_Split)
+    print(Sentence_Count)
+    
+
+

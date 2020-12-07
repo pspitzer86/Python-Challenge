3a63bbcc4a16725681d5f6f94e1ca61a9cea034b
Patrick Spitzer <rick.spitzer@yahoo.com>
2020-12-06 17:34:15 -0700

    Attempting to count letters, added final print statements and writing output file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -16,9 +16,34 @@ with open(my_paragraph, 'r') as text:
     Word_Count = len(Word_Split)
     
     Sentence_Count = len(re.findall(r'\.', lines))
-    #Sentence_Split = lines.split(".")
-    #Sentence_Count = len(Sentence_Split)
-    print(Sentence_Count)
+
+    Letter_Count = re.split("(?<=[.,!? ]) +", lines)
+    print(Letter_Count)
+
+
+
+
+print("\nParagraph Analysis")
+print("--------------------------")
+print(f"Approximate Word Count : {Word_Count}")
+print(f"Approximate Sentence Count : {Sentence_Count}")
+#print(f"Average Letter Count : {}")
+#print(f"Average Sentence Length : {}")
+
+
+# Set variable for output file
+
+output_file = "C:\\Python-Challenge\\PyParagraph\\Analysis\\paragraph_analysis.txt"
+
+#  Open the output file in the same format as printed in terminal
+
+with open(output_file, "w") as text_file:
+    text_file.write("Paragraph Analysis\n")
+    text_file.write("--------------------------\n")
+    text_file.write(f'Approxiate Word Count : {Word_Count}\n')
+    text_file.write(f'Approximate Sentence Count : {Sentence_Count}\n')
+    #text_file.write(f'Average Letter Count : {}\n')
+    #text_file.write(f'Average Sentence Length : {}\n')
     
 
 
